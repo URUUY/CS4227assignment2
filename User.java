19e8d7adaaa515619a20275584b3561d170752db
URUUY <Suen112230@outlook.com>
2025-03-15 00:39:50 +0000

    delete backend

--- User.java
+++
@@ -1,9 +0,0 @@
-@Entity
-@Data
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String username;
-    private String password; // 加密后存储
-}
